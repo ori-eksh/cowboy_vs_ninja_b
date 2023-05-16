8a9f9bf25d3e3753bb9e6de37bf8050368c18a75
Ori <mymail@gmail.com>
2023-05-16 19:08:52 +0300

    oti

--- sources/Character.hpp
+++ sources/Character.hpp
@@ -62,6 +62,10 @@ namespace ariel
         {
             return played;
         }
+        void setLocation(Point &poi)
+        {
+            location = poi;
+        }
     };
 }
 //
--- sources/Ninja.hpp
+++ sources/Ninja.hpp
@@ -52,11 +52,6 @@ namespace ariel
         void move(Character *target);
         std::string print() override;
         ~Ninja() override {}
-        void setLocation(Point poi)
-        {
-            this->getLocation().setX(poi.getX());
-            this->getLocation().setY(poi.getY());
-        }
     };
 
 }
